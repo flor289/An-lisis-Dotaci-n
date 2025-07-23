efe1d302884979877fed97a338faa093214bf629
flor289 <103067613+flor289@users.noreply.github.com>
2025-07-23 08:43:31 -0300

    Update analisis_dotacion.py

--- analisis_dotacion.py
+++ analisis_dotacion.py
@@ -61,7 +61,7 @@ def crear_pdf_resumen(n_altas, n_bajas, df_bajas_motivo, df_resumen_activos):
             pdf.ln()
 
     # Retorna el PDF como bytes
-    return pdf.output(dest='S')
+    return bytes(pdf.output())
 
 # --- CONFIGURACIÓN E INTERFAZ DE LA APP ---
 st.set_page_config(page_title="Dashboard de Dotación", layout="wide")
